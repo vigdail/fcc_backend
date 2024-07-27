3c0c242fa0d8d5a191ec74a9f3f693d8a906180e
vigdail <vigdail.dev@gmail.com>
2024-07-27 21:54:09 +0500

    File Metadata Microservice complete

--- filemetadata/index.js
+++ filemetadata/index.js
@@ -1,20 +1,25 @@
-var express = require('express');
-var cors = require('cors');
 require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
 
-var app = express();
+const app = express();
+
+const upload = multer({dest: 'uploads/'});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+    const file = req.file;
+    res.json({name: file.originalname, type: file.mimetype, size: file.size});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+    console.log('Your app is listening on port ' + port)
 });
